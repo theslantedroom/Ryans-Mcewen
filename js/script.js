de18d647b33f79ee1e2e543c900211ca940b141e
theslantedroom <73557008+theslantedroom@users.noreply.github.com>
2020-11-22 22:49:48 -0800

    1

---
+++ js/script.js
@@ -0,0 +1,21 @@
+document.getElementById("myBtn").addEventListener("click", function() {
+  document.getElementById('afraid').innerText = 'DEMANDING RESPECT - SHOWING NO MERCY';
+});
+
+
+document.querySelectorAll(".steveButton")[0].addEventListener("click", function(){
+	alert('Yeah Your Boy');
+});
+
+document.querySelectorAll(".steveButton")[1].addEventListener("click", function(){
+	alert('100% LEGAL');
+});
+
+document.querySelectorAll(".steveButton")[2].addEventListener("click", function(){
+	alert('Saving the world');
+});
+
+document.querySelectorAll(".steveButton")[3].addEventListener("click", function(){
+	alert('100% LEGAL');
+});
+
