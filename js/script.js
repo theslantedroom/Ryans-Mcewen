963a5a79229474cd782cda324106f6d83a080454
Steve Yee <steveplaysaxe@hotmail.com>
2024-11-23 14:42:41 -0800

    40th

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
-document.getElementById("myBtn").addEventListener("click", function() {
-  document.getElementById('afraid').innerText = 'DEMANDING RESPECT - SHOWING NO MERCY';
+document.getElementById("myBtn").addEventListener("click", function () {
+  document.getElementById("afraid").innerText = "PERVIN HARD - FUCKING COWS";
 });
 
+document
+  .querySelectorAll(".steveButton")[0]
+  .addEventListener("click", function () {
+    alert("We evaluate opportunities for success, and succeed at that, DAILY");
+  });
 
-document.querySelectorAll(".steveButton")[0].addEventListener("click", function(){
-	alert('We evaluate opportunities for success, and succeed at that, DAILY');
-});
+document
+  .querySelectorAll(".steveButton")[1]
+  .addEventListener("click", function () {
+    alert("100% LEGAL");
+  });
 
-document.querySelectorAll(".steveButton")[1].addEventListener("click", function(){
-	alert('100% LEGAL');
-});
+document
+  .querySelectorAll(".steveButton")[2]
+  .addEventListener("click", function () {
+    alert("Solve problems with easy and protege level mastery");
+  });
 
-document.querySelectorAll(".steveButton")[2].addEventListener("click", function(){
-	alert('Solve problems with easy and protege level mastery');
-});
-
-document.querySelectorAll(".steveButton")[3].addEventListener("click", function(){
-	alert('100% LEGAL');
-});
-
+document
+  .querySelectorAll(".steveButton")[3]
+  .addEventListener("click", function () {
+    alert("100% LEGAL");
+  });
